d05b0c7ce7a65e5745d4b0e2c6ee61f4509417e8
Jerycho Ramadan <ramadhanjerycho@gmail.com>
2025-11-20 14:24:07 +0700

    Commit Pertama

---
+++ app/src/main/java/com/example/owelpay/registerPage2.kt
@@ -0,0 +1,23 @@
+package com.example.owelpay
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class registerPage2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_page2)
+
+        val mainLayout = findViewById<ConstraintLayout>(R.id.no_hp)
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
