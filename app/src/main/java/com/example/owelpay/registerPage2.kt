c09679b439501178ffd1c36619424303a0b3eb56
Jerycho Ramadan <ramadhanjerycho@gmail.com>
2025-11-21 08:54:00 +0700

    Commit kedua

--- app/src/main/java/com/example/owelpay/registerPage2.kt
+++ app/src/main/java/com/example/owelpay/registerPage2.kt
@@ -1,11 +1,15 @@
 package com.example.owelpay
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+
 
 class registerPage2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +23,37 @@ class registerPage2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btn_daftar = findViewById<Button>(R.id.btn_daftar)
+
+
+        btn_daftar.setOnClickListener {
+            val inputNumber = findViewById<EditText>(R.id.input_number)
+            val phone = inputNumber.text.toString().trim()
+
+            // VALIDASI: kosong?
+            if (phone.isEmpty()) {
+                inputNumber.error = "Nomor HP tidak boleh kosong"
+                return@setOnClickListener
+            }
+
+            // VALIDASI: kurang dari 10 digit? (aturan Indonesia)
+            if (phone.length < 10) {
+                inputNumber.error = "Nomor HP tidak valid"
+                return@setOnClickListener
+            }
+
+            // VALIDASI: harus angka semua
+            if (!phone.matches(Regex("^[0-9]+$"))) {
+                inputNumber.error = "Nomor HP hanya boleh angka"
+                return@setOnClickListener
+            }
+
+            // Kalau lolos validasi → lanjut ke OTP
+            val intent = Intent(this, kodeOtp::class.java)
+            intent.putExtra("nomor_hp", phone)  // kirim nomor HP
+            startActivity(intent)
+        }
+
     }
 }
